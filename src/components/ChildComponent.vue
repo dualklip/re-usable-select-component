e7022474835d09b5d8146e114104ef0b69a93a58
dualklip <ml@phoenixconnection.es>
2024-04-08 17:38:43 +0200

    Initial commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+	props: {
+		options: Array
+	},
+	setup(props) {
+		const onSelectChange = (e) => {
+			props.options.forEach((option) => {
+				option.selected = (option.label === e.target.value);
+			});
+		};
+		return { onSelectChange };
+	}
+};
+</script>
+
+<template>
+	<select @change="onSelectChange">
+		<option v-for="(option, index) in options" :key="index"
+		        :value="option.label" :selected="option.selected">
+			{{ option.label }}
+		</option>
+	</select>
+</template>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,25 @@
+<script>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+export default {
+	components: {
+		ChildComponent
+	},
+	setup () {
+		const dataForm = ref({
+			options: [
+				{id: 1, label: 'Option 1', selected: false},
+				{id: 2, label: 'Option 2', selected: false},
+				{id: 3, label: 'Option 3', selected: false}
+			]
+		});
+		return {dataForm};
+	}
+};
+</script>
+
+<template>
+	<ChildComponent :options="dataForm.options"></ChildComponent>
+	<div>{{dataForm.options}}</div>
+</template>
